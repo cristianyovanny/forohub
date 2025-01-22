8870287707b0e76a07afef88e62d42c8bc3469a5
Cristian Yovanny <cjimenezd90@gmail.com>
2025-01-21 22:55:41 -0500

    Update

--- src/main/java/com/alura/forohub/ForohubApplication.java
+++ src/main/java/com/alura/forohub/ForohubApplication.java
@@ -1,4 +1,4 @@
-package com.alejobeliz.proyectos.forohub;
+package com.alura.forohub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
